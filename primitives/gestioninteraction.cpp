1ebeabce03e36fa4b00ed651d8a927285973bc76
Gibus21250 <jacquinet.nathan@gmail.com>
2022-12-15 23:40:01 +0100

    Ajout de fonctions demandée dans le bareme

--- primitives/gestioninteraction.cpp
+++ primitives/gestioninteraction.cpp
@@ -17,3 +17,43 @@ bool GestionInteraction::supprimerParId(const unsigned IdC)
     }
     return finded;
 }
+
+GestionInteraction& GestionInteraction::getInteractionsContenant(const std::string &mot)
+{
+
+    //On parcour toutes les tâches
+    for(auto it = m_list.begin(); it != m_list.end(); ++it){
+        //Si le contenu de la tache ne contient pas le mot recherché, on le supprime
+        if(it->getContenu().find(mot) == std::string::npos)
+        {
+            m_list.erase(it);           //On le supprime de la list
+        }
+    }
+    return *this;
+}
+
+GestionInteraction& GestionInteraction::getInteractionsAvant(const DateSimple &date)
+{
+    //On parcour toutes les tâches
+    for(auto it = m_list.begin(); it != m_list.end(); ++it){
+        //Si le contenu de la tache ne contient pas le mot recherché, on le supprime
+        if(it->getDate() > date)
+        {
+            m_list.erase(it);           //On le supprime de la list
+        }
+    }
+    return *this;
+}
+
+GestionInteraction& GestionInteraction::getInteractionsApres(const DateSimple &date)
+{
+    //On parcour toutes les tâches
+    for(auto it = m_list.begin(); it != m_list.end(); ++it){
+        //Si le contenu de la tache ne contient pas le mot recherché, on le supprime
+        if(it->getDate() < date)
+        {
+            m_list.erase(it);           //On le supprime de la list
+        }
+    }
+    return *this;
+}
--- primitives/gestioninteraction.h
+++ primitives/gestioninteraction.h
@@ -17,6 +17,27 @@ public:
     ///Supprime l'instance Interaction associé à l'ID en paramètre
     bool supprimerParId(const unsigned IdI);
 
+    /**
+     * @brief Permet de récupérer une instance de GestionInteraction avec dedans toutes les Interactions contenant le mot 'mot'
+     * @param mot à tester
+     * @return Un nouveau GestionInteraction contenant des Interactions satisfaisant la condition
+     */
+    GestionInteraction& getInteractionsContenant(const std::string& mot);
+
+    /**
+     * @brief Permet de récupérer une instance de GestionInteraction avec dedans toutes les Interactions se passant avant la date en agument
+     * @param La date
+     * @return Un nouveau GestionInteraction contenant des Interactions satisfaisant la condition
+     */
+    GestionInteraction& getInteractionsAvant(const DateSimple& date);
+
+    /**
+     * @brief Permet de récupérer une instance de GestionTache avec dedans toutes les Interactions se passant après la date en agument
+     * @param La date
+     * @return Un nouveau GestionInteraction contenant des Interactions satisfaisant la condition
+     */
+    GestionInteraction& getInteractionsApres(const DateSimple& date);
+
 };
 
 #endif // GESTIONINTERACTION_H
